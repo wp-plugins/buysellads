034b1eef7eb288c06d63d65eb53ababc1f52c52c
barchard <barchard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-30 21:13:09 +0000

    Localization for site key should not mention BSA.
    git-svn-id: https://plugins.svn.wordpress.org/buysellads/tags/1.1@317142 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- language/es_ES/admin_lang.php
+++ language/es_ES/admin_lang.php
@@ -14,7 +14,7 @@ $lang['widget_ad_zone'] = 'Ad Zone';
 $lang['settings_updated'] = 'Settings <strong>Updated</strong>.';
 $lang['settings_error'] = '<strong>Error</strong>! Your Site Key could not be verified.';
 $lang['site_key'] = 'Site Key';
-$lang['site_key_desc'] = 'Paste your <a href="http://buysellads.com/publishers" rel="external">BuySellAds.com</a> Site Key here.';
+$lang['site_key_desc'] = 'Copy+paste your Site Key here. You can find this on the "install ad code" page.';
 $lang['empty_site_key'] = 'A Site key has not been added yet. <a href="%s">Add One</a>.';
 $lang['bsa_body_open'] = 'Asynchronous Code';
 $lang['bsa_body_open_desc'] = 'Directly after the opening <strong>&lt;body&gt;</strong> tag insert <strong>&lt;?php if (function_exists(\'wp_body_open\')) { wp_body_open(); } ?&gt;</strong>. Otherwise, the necessary code snippet will be inserted using <strong>&lt;?php wp_footer(); ?&gt;</strong>';

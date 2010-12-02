dfc860f924c9fc5d55b1b72efeb61b842f777db2
barchard <barchard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-02 22:06:28 +0000

    Bringing everything up to date.
    git-svn-id: https://plugins.svn.wordpress.org/buysellads/tags/1.2.2@318221 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- buysellads.php
+++ buysellads.php
@@ -3,7 +3,7 @@
 Plugin Name: BuySellAds
 Plugin URI: http://buysellads.com/
 Description: Official BuySellAds.com WordPress plugin.
-Version: 1.1
+Version: 1.2.2
 Author: Derek Herman
 Author URI: http://valendesigns.com/
 License: GPL3
